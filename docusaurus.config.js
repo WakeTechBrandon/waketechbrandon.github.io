5cf720addd8e2eef9137171e8ee93cdc62b367f6
Brandon Biggs <71558468+TreasuredQuartz@users.noreply.github.com>
2025-07-10 14:21:50 -0400

    Second Commit: Establish Docusaurus

---
+++ docusaurus.config.js
@@ -0,0 +1,14 @@
+export default {
+    title: 'Brandon M. Biggs', // Title for your website.
+    tagline: 'Future Game developer, present programmer and technical artist.',
+    // profilePhoto: 'img/PortfolioBackground.png',
+    url: 'https://WakeTechBrandon.github.io', // Your website URL
+    baseUrl: '/tech-portfolio/', // Base URL for your project */
+    // For github.io type URLs, you would set the url and baseUrl like:
+    //   url: 'https://facebook.github.io',
+    //   baseUrl: '/test-site/',
+
+    // Used for publishing and more
+    projectName: 'tech-portfolio',
+    organizationName: 'WakeTechBrandon',
+};
